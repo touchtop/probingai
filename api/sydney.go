073a7afdc107c23e8f7559a2e565001e49773938
jokyo3 <150494320+jokyo3@users.noreply.github.com>
2023-11-23 13:19:38 +0800

    Update sydney.go

--- api/sydney.go
+++ api/sydney.go
@@ -4,20 +4,20 @@ import (
 	"adams549659584/go-proxy-bingai/api/helper"
 	"adams549659584/go-proxy-bingai/common"
 	"net/http"
-	"os"
-	"net/url" 
+//	"os"
+//	"net/url" 
 )
 
-var sydneyURL = os.Getenv("SYDNEY_PROXY_DM")
+//var sydneyURL = os.Getenv("SYDNEY_PROXY_DM")
 
 func Sydney(w http.ResponseWriter, r *http.Request) {
 	if !helper.CheckAuth(r) {
 		helper.UnauthorizedResult(w)
 		return
 	}
-	if sydneyURL == "" {
-		sydneyURL = common.BING_SYDNEY_URL.String()
-	}
-	sydproxyurl, _ := url.Parse(sydneyURL)
-	common.NewSingleHostReverseProxy(sydproxyurl).ServeHTTP(w, r)
+//	if sydneyURL == "" {
+//		sydneyURL = common.BING_SYDNEY_URL.String()
+//	}
+//	sydproxyurl, _ := url.Parse(sydneyURL)
+	common.NewSingleHostReverseProxy(common.sydneyURL).ServeHTTP(w, r)
 }
