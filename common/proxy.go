9659b98edfea54308b41d3c6bb4f54d08f9ae5fc
jokyo3 <150494320+jokyo3@users.noreply.github.com>
2023-11-23 15:12:35 +0800

    Update proxy.go

--- common/proxy.go
+++ common/proxy.go
@@ -26,8 +26,8 @@ var (
 	BING_SYDNEY_URL, _  = url.Parse(BING_SYDNEY_DOMAIN)
 	BING_URL, _         = url.Parse("https://www.bing.com")
 	EDGE_SVC_URL, _     = url.Parse("https://edgeservices.bing.com")
-	bingURL,_	= ""
-	sydneyURL,_	= ""
+	bingURL			*url.URL
+	sydneyURL		*url.URL
 	KEEP_REQ_HEADER_MAP = map[string]bool{
 		"Accept":                         true,
 		"Accept-Encoding":                true,
